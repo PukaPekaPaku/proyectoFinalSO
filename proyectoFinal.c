b3b9a92a1763c96a5c3e6d0c37856be4b31f93b5
AMD <pukapekapaku@gmail.com>
2024-05-17 16:34:50 -0600

    Agregada la lectura de directorios

--- proyectoFinal.c
+++ proyectoFinal.c
@@ -7,15 +7,25 @@
 #include <curses.h>
 #include <sys/stat.h>
 #include <sys/mman.h>
+#include <math.h>
 
 #include "ext4.h"
 
 /* Variables globales */
 int fd;  /* Archivo a leer */
 long fs; /* Tamaño de archivo */
-int term_x, term_y;
+char *ptr;
 
 /* Funciones */
+char *mapFile(char *filePath);
+void showMenu();
+void particiones(char *ptr);
+void superbloque(char *ptr);
+void showGDT(char *ptr);
+void gdt(char *ptr);
+// void lectura_inode(char *ptr, struct ext4_inode inode, unsigned short offset);
+void lee_inode(struct ext4_inode *ptrInode, char *ptr);
+
 char *mapFile(char *filePath)
 {
     /* Abre archivo */
@@ -45,14 +55,10 @@ char *mapFile(char *filePath)
 void showMenu()
 {
     clear();
-    move(1, 1);
-    addstr("Opciones");
-    move(3, 1);
-    addstr("1: Particiones\n");
-    move(4, 1);
-    addstr("2: Superblock\n");
-    move(5, 1);
-    addstr("3: Explorar particion");
+    mvprintw(1, 1, "Opciones");
+    mvprintw(3, 1, "1: Particiones\n");
+    mvprintw(4, 1, "2: Superblock particion Linux\n");
+    mvprintw(5, 1, "3: Explorar particion Linux\n");
 
     move(getmaxy(stdscr) - 4, 1);
     addstr("Controles:\n  ^ v   : navegar.\n  ENTER : elegir seleccion.\n  'q'   : salir");
@@ -60,23 +66,23 @@ void showMenu()
 
 void particiones(char *ptr)
 {
-    unsigned char hi, si, hf, sf;
+    unsigned char hi, si, hf, sf, type;
     unsigned short ci, cf;
-    unsigned int sector, size;
-    char buffer[20], *checkpoint;
+    __le32 sector, size;
+    char *checkpoint;
 
     clear();
     move(1, 1);
-    addstr("Particion  Ci   Hi   Si   Sector inicio   Cf   Hf   Sf   Tamaño\n");
+    addstr("Particion  Ci   Hi   Si   Sector inicio   Cf   Hf   Sf   Tamaño   Tipo\n");
 
     for (int row = 2; row < 6; row++)
     {
 
+        checkpoint = ptr;
         /**
          * Status
          * 0x00
          */
-        checkpoint = ptr;
 
         ptr++;
         /**
@@ -105,6 +111,7 @@ void particiones(char *ptr)
          * Partition type
          * 0x04
          */
+        type = *ptr;
 
         ptr++;
         /**
@@ -137,7 +144,7 @@ void particiones(char *ptr)
          * 0x0A
          * 0x0B
          */
-        sector = (unsigned char)*ptr | ((unsigned char)*(ptr + 1) << 8) | ((unsigned char)*(ptr + 2) << 16) | ((unsigned char)*(ptr + 3) << 24);
+        sector = *(__le32 *)ptr;
 
         ptr += 4;
         /**
@@ -148,142 +155,265 @@ void particiones(char *ptr)
          * 0x0E
          * 0x0F
          */
-        size = (unsigned char)*ptr | ((unsigned char)*(ptr + 1) << 8) | ((unsigned char)*(ptr + 2) << 16) | ((unsigned char)*(ptr + 3) << 24);
+        size = *(__le32 *)ptr;
 
-        //  Particion  Ci   Hi   Si   Sector inicio   Cf   Hf   Sf   Tamaño\n
-        // 0_________0_2____7__0_2____7__0_________0__3____8_0__3____8
-        move(row, 12);
-        sprintf(buffer, "%hu", ci);
-        addstr(buffer);
-        memset(buffer, 0, sizeof(buffer));
+        //  Particion  Ci   Hi   Si   Sector inicio   Cf   Hf   Sf   Tamaño   Tipo\n
+        // 0_________0_2____7__0_2____7__0_________0__3____8_0__3____8_0______7
+        mvprintw(row, 12, "%hu", ci);
 
-        move(row, 17);
-        sprintf(buffer, "%u", hi);
-        addstr(buffer);
-        memset(buffer, 0, sizeof(buffer));
+        mvprintw(row, 17, "%u", hi);
 
-        move(row, 22);
-        sprintf(buffer, "%u", si);
-        addstr(buffer);
-        memset(buffer, 0, sizeof(buffer));
+        mvprintw(row, 22, "%u", si);
 
-        move(row, 27);
         if (sector)
         {
-            sprintf(buffer, "%u", sector);
-            addstr(buffer);
-            memset(buffer, 0, sizeof(buffer));
+            mvprintw(row, 27, "%u", sector);
         }
         else
         {
-            addstr("Sin particion");
+            mvprintw(row, 27, "-");
         }
 
-        move(row, 43);
-        sprintf(buffer, "%hu", cf);
-        addstr(buffer);
-        memset(buffer, 0, sizeof(buffer));
+        mvprintw(row, 43, "%hu", cf);
 
-        move(row, 48);
-        sprintf(buffer, "%u", hf);
-        addstr(buffer);
-        memset(buffer, 0, sizeof(buffer));
+        mvprintw(row, 48, "%hu", hf);
 
-        move(row, 53);
-        sprintf(buffer, "%u", sf);
-        addstr(buffer);
-        memset(buffer, 0, sizeof(buffer));
+        mvprintw(row, 53, "%hu", sf);
 
-        move(row, 58);
-        sprintf(buffer, "%u", size);
-        addstr(buffer);
-        memset(buffer, 0, sizeof(buffer));
+        mvprintw(row, 58, "%u", size);
+
+        mvprintw(row, 67, "%X%c", type, 'h');
 
         ptr = checkpoint + 0x10;
     }
 
     move(getmaxy(stdscr) - 2, 1);
-    addstr("Controles:\n  'q'   : salir");
+    addstr("Controles:\n  BACKSPACE : atras");
 
     int c = getch();
-    while (c != 'q')
+    while (c != KEY_BACKSPACE)
     {
         c = getch();
-        /* Exit on 'q' */
+        /* Exit on BACKSPACE */
     }
 }
 
 void superbloque(char *ptr)
 {
     int row = 1;
-    char buffer[80];
 
     clear();
     move(row++, 1);
 
     addstr("Superblock\n");
-    struct ext4_super_block *superbloque = (struct ext4_super_block *)ptr;
+    struct ext4_super_block superbloque;
+    memcpy(&superbloque, &ptr[0 + 0x400], sizeof(struct ext4_super_block));
 
-    move(++row, 1);
-    sprintf(buffer, "%s%u\n", "Recuento Inodes: ", superbloque->s_inodes_count);
-    addstr(buffer);
-    memset(buffer, 0, sizeof(buffer));
+    mvprintw(++row, 1, "Recuendo Inodes: %u", superbloque.s_inodes_count);
 
-    move(++row, 1);
-    sprintf(buffer, "%s%u\n", "Recuento Blocks: ",superbloque->s_blocks_count_lo);
-    addstr(buffer);
-    memset(buffer, 0, sizeof(buffer));
+    mvprintw(++row, 1, "Recuento Blocks: %u", superbloque.s_blocks_count_lo);
 
-    move(++row, 1);
-    sprintf(buffer, "%s%s\n", "Etiqueta: ",superbloque->s_volume_name);
-    addstr(buffer);
-    memset(buffer, 0, sizeof(buffer));
+    mvprintw(++row, 1, "Etiqueta: %s", superbloque.s_volume_name);
 
-    move(++row, 1);
-    sprintf(buffer, "%s%s\n", "Montado por ultima vez en: ",superbloque->s_last_mounted);
-    addstr(buffer);
-    memset(buffer, 0, sizeof(buffer));
+    mvprintw(++row, 1, "Montado por ultima vez en: %s", superbloque.s_last_mounted);
 
-    move(++row, 1);
-    sprintf(buffer, "%s%u\n", "Primer Inode: ",superbloque->s_first_ino);
-    addstr(buffer);
-    memset(buffer, 0, sizeof(buffer));
+    mvprintw(++row, 1, "Primer Inode: %u", superbloque.s_first_ino);
 
-    move(++row, 1);
-    sprintf(buffer, "%s%u\n", "Block size: ",sizeof(struct ext4_super_block));
-    addstr(buffer);
-    memset(buffer, 0, sizeof(buffer));
+    mvprintw(++row, 1, "Block size: %u", (unsigned short)pow((double)2, (double)(10 + superbloque.s_log_block_size)));
 
-    move(++row, 1);
-    sprintf(buffer, "%s%u\n", "Inode size: ",superbloque->s_inode_size);
-    addstr(buffer);
-    memset(buffer, 0, sizeof(buffer));
+    mvprintw(++row, 1, "Inode size: %u", superbloque.s_inode_size);
 
-    move(++row, 1);
-    sprintf(buffer, "%s%u\n", "Blocks per group: ",superbloque->s_blocks_per_group);
-    addstr(buffer);
-    memset(buffer, 0, sizeof(buffer));
+    mvprintw(++row, 1, "Blocks per group: %u", superbloque.s_blocks_per_group);
 
-    move(++row, 1);
-    sprintf(buffer, "%s%u\n", "Inodes per group: ",superbloque->s_inodes_per_group);
-    addstr(buffer);
-    memset(buffer, 0, sizeof(buffer));
+    mvprintw(++row, 1, "Inodes per group: %u", superbloque.s_inodes_per_group);
 
-    move(++row, 1);
-    sprintf(buffer, "%s%X\n", "Magic number: ",superbloque->s_magic);
-    addstr(buffer);
-    memset(buffer, 0, sizeof(buffer));
+    mvprintw(++row, 1, "Magic number: %X", superbloque.s_magic);
 
     /* END */
-
     move(getmaxy(stdscr) - 2, 1);
-    addstr("Controles:\n  'q'   : salir");
+    addstr("Controles:\n  BACKSPACE : atras");
 
     int c = getch();
-    while (c != 'q')
+    while (c != KEY_BACKSPACE)
     {
         c = getch();
-        /* Exit on 'q' */
+        /* Exit on BACKSPACE */
+    }
+}
+
+void showGDT(char *ptr)
+{
+    int row = 1;
+    clear();
+    move(row++, 1);
+    addstr("Primer Block Group Descriptor\n");
+
+    struct ext4_group_desc gdt;
+    memcpy(&gdt, &ptr[0 + 0x800], sizeof(struct ext4_group_desc));
+
+    mvprintw(++row, 1, "Recuendo Inodes: %hu", gdt.bg_used_dirs_count_lo);
+
+    mvprintw(++row, 1, "Bitmap block: %u", gdt.bg_block_bitmap_lo);
+
+    mvprintw(++row, 1, "Inode table block: %u", gdt.bg_inode_table_lo);
+
+    /* END */
+    move(getmaxy(stdscr) - 3, 1);
+    addstr("Controles:\n  ENTER     : siguiente\n  BACKSPACE : atras");
+}
+
+void gdt(char *ptr)
+{
+    struct ext4_group_desc gdt;
+    memcpy(&gdt, &ptr[0 + 0x800], sizeof(struct ext4_group_desc));
+
+    struct ext4_inode inode;
+    memcpy(&inode, (ptr + (0x400 * gdt.bg_inode_table_lo) + 0x100), sizeof(struct ext4_inode));
+
+    showGDT(ptr);
+
+    int c = getch();
+    while (1)
+    {
+        switch (c)
+        {
+        case KEY_BACKSPACE:
+            return;
+            break;
+
+        case 10:
+            lee_inode(&inode, ptr);
+            break;
+
+        default:
+            break;
+        }
+
+        showGDT(ptr);
+
+        c = getch();
+    }
+}
+
+void lee_inode(struct ext4_inode *ptrInode, char *ptr)
+{
+    if (((ptrInode->i_mode) & 0x4000) == 0x4000)
+    {
+        /* Estamos leyendo un directorio */
+        struct ext4_extent_header eh;
+        memcpy(&eh, &(ptrInode->i_block[0]), sizeof(struct ext4_extent_header));
+
+        /* struct ext4_extent ex[eh.eh_max];
+
+        int i = 0;
+        for(i = 0; i < eh.eh_entries; i++) {
+            memcpy(&(ex[i]), &(ptrInode->i_block[(i + 1) * 3]), sizeof(struct ext4_extent));
+        } */
+
+        struct ext4_extent ex;
+        memcpy(&ex, &(ptrInode->i_block[3]), sizeof(struct ext4_extent));
+
+        unsigned short numEntries = 0;
+        struct ext4_dir_entry_2 auxDirEntry;
+
+        char *ptrDirs = (ptr + (ex.ee_start_lo * 0x400));
+
+        memcpy(&auxDirEntry, ptrDirs, sizeof(struct ext4_dir_entry_2));
+
+        while (auxDirEntry.inode != 0)
+        {
+            numEntries++;
+            ptrDirs += auxDirEntry.rec_len;
+            memcpy(&auxDirEntry, ptrDirs, sizeof(struct ext4_dir_entry_2));
+        }
+
+        ptrDirs = (ptr + (ex.ee_start_lo * 0x400));
+        struct ext4_dir_entry_2 dirEntries[numEntries];
+
+        int i = 0;
+        while (i < numEntries)
+        {
+            memcpy(&dirEntries[i], ptrDirs, sizeof(struct ext4_dir_entry_2));
+            ptrDirs += dirEntries[i].rec_len;
+            i++;
+        }
+
+        /* ----------------------------------------------------------------------------------------------- */
+        int row = 0;
+        clear();
+        move(++row, 1);
+        addstr("Directorio");
+        row++;
+
+        int j = 0;
+        while (j < numEntries)
+        {
+            mvprintw(++row, 1, "%s -> %u", dirEntries[j].name, dirEntries[j].inode);
+            j++;
+        }
+
+        /* END */
+        move(getmaxy(stdscr) - 4, 1);
+        addstr("Controles:\n  ^ v       : navegar.\n  ENTER     : elegir seleccion.\n  BACKSPACE : atras");
+        row = 3;
+        move(row, 1);
+        /* ----------------------------------------------------------------------------------------------- */
+
+        int c = getch();
+        while (1)
+        {
+            switch (c)
+            {
+            case KEY_BACKSPACE: /* Exit on BACKSPACE */
+                return;
+                break;
+
+            case KEY_UP:
+                if (row > 3)
+                {
+                    row--;
+                }
+                break;
+
+            case KEY_DOWN:
+                if ((row - 2) < numEntries)
+                {
+                    row++;
+                }
+                break;
+
+            case 10:
+                /* Elemento seleccionado */
+                unsigned int gdOfSelected = (dirEntries[row - 3].inode - 1) / 0x7F8;
+                unsigned int offsetOfSelected = (dirEntries[row - 3].inode - 1) % 0x7F8;
+
+                struct ext4_group_desc gd;
+                memcpy(&gd, (ptr + 0x800 + (gdOfSelected * sizeof(struct ext4_group_desc))), sizeof(struct ext4_group_desc));
+                
+                struct ext4_inode inodeN;
+                memcpy(&inodeN, (ptr + (0x400 * gd.bg_inode_table_lo) + (offsetOfSelected * 0x100)), sizeof(struct ext4_inode));
+
+                lee_inode(&inodeN, ptr);
+                
+                return;
+                break;
+
+            default:
+                break;
+            }
+            move(row, 1);
+            c = getch();
+        }
+    }
+    else if (((ptrInode->i_mode) & 0x8000) == 0x8000)
+    {
+        /* Estamos leyendo un archivo */
+
+        // TODO abrir en hex y permitir guardar
+    }
+    else
+    {
+        return;
     }
 }
 
@@ -301,7 +431,7 @@ int main(int argc, char const *argv[])
     noecho();
 
     /* Mapeo de archivo */
-    char *ptr = mapFile(argv[1]);
+    ptr = mapFile(argv[1]);
     if (ptr == NULL)
     {
         exit(EXIT_FAILURE);
@@ -343,7 +473,11 @@ int main(int argc, char const *argv[])
                 break;
             case 2:
                 // Hard-coded :/
-                superbloque(ptr + 0x100400);
+                superbloque(ptr + 0x100000);
+                break;
+
+            case 3:
+                gdt(ptr + 0x100000);
                 break;
 
             default:
@@ -354,7 +488,7 @@ int main(int argc, char const *argv[])
             break;
 
         default:
-            
+
             break;
         }
         move(row, col);
